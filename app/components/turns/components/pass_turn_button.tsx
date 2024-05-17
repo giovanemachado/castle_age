6fd74f701ef2d54dcc1dd8562cac1cc7b7a4238c
giovanemachado <giovanemachado3@gmail.com>
2024-05-17 18:53:55 -0300

    update pass turn button

--- app/components/turns/components/pass_turn_button.tsx
+++ app/components/turns/components/pass_turn_button.tsx
@@ -4,17 +4,21 @@ import { useGameStore } from "@/app/store/gameStoreProvider";
 import { fetchData } from "@/utils/requests";
 
 const PassTurnButton = () => {
-  const { turns, units, gameId, playerIds, money, passTurn } = useGameStore(
+  const { turns, units, money, passTurn, match } = useGameStore(
     (state) => state,
   );
 
   const handleClick = async () => {
-    const { data } = await fetchData("token", `games/${gameId}/state`, "POST", {
-      playerIds,
-      turns,
-      units,
-      money,
-    });
+    const { data } = await fetchData(
+      "token",
+      `games/turn/${match.code}`,
+      "POST",
+      {
+        turns,
+        units,
+        money,
+      },
+    );
 
     passTurn(data);
   };
