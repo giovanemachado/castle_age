ebbb77c87ad786948e0f02899a648312c79880a8
giovanemachado <giovanemachado3@gmail.com>
2024-06-22 13:55:59 -0300

    show priority player for debugging reasons

--- app/components/gameplay/turns/turns.tsx
+++ app/components/gameplay/turns/turns.tsx
@@ -4,10 +4,17 @@ import { useGameStore } from "@/app/store/gameStoreProvider";
 import PassTurnButton from "./components/pass_turn_button";
 
 const Turns = () => {
-  const { turns } = useGameStore((state) => state);
+  const { turns, match, player } = useGameStore((state) => state);
+
+  // TODO temp, move this to the correct spot
+  const prioPlayer = turns % 2 === 0 ? match?.players[0] : match?.players[1];
 
   return (
     <div className="flex py-2 justify-end w-full items-center">
+      {/* TODO temp, move this to the correct spot */}
+      {prioPlayer == player?.playerId && (
+        <span className="px-2 ">Player priority</span>
+      )}
       <span className="px-2 ">{`Turn: ${turns}`}</span>
       <PassTurnButton />
     </div>
